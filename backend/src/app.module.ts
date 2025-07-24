13729e4ef07d73a6cbeca1757825e201f73d4a69
Justin Scott <Jstman2000@gmail.com>
2025-07-24 13:23:28 -0400

    chore: initialize NestJS backend with customers resource scaffolded

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomersModule } from './customers/customers.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    CustomersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
