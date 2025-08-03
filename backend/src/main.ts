66521547b84baeb83744b8481045b4f8429fe4ac
Justin Scott <Jstman2000@gmail.com>
2025-08-03 15:07:09 -0400

    chore: flyctl launched

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.useGlobalFilters(new HttpExceptionFilter() satisfies ExceptionFilter);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap().catch((error) => {
   console.error('Bootstrap error:', error);
