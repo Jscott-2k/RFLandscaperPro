814e15d6ff2e398decd0d384abb03fe41d56b249
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-19 15:19:52 -0400

    feat: add job scheduling details

--- backend/src/jobs/dto/update-job.dto.ts
+++ backend/src/jobs/dto/update-job.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateJobDto } from './create-job.dto';
 
+// Extends CreateJobDto with all fields optional
 export class UpdateJobDto extends PartialType(CreateJobDto) {}
