a002ccc33bde3f8d328eeb7dcc67a9b2b7dd6056
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-19 21:22:13 -0400

    Restrict user creation to admins

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Roles } from '../common/decorators/roles.decorator';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './user.entity';
+import { User, UserRole } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(UserRole.Admin)
   @Post()
   create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
