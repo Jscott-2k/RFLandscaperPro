62158f3339ae94b0c11e882fd5a52654f3ed9238
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-27 18:21:06 -0400

    fix: return promise in metrics guard

--- backend/src/common/guards/metrics-throttler.guard.ts
+++ backend/src/common/guards/metrics-throttler.guard.ts
@@ -4,7 +4,7 @@ import { Request } from 'express';
 
 @Injectable()
 export class MetricsThrottlerGuard extends ThrottlerGuard {
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest<Request>();
     if (req.path?.startsWith('/metrics')) {
       return true;
