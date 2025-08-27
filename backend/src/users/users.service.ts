9dd72a1d4586382222daf47b4b8f99f0be9c855e
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-27 13:19:54 -0400

    feat: use nodemailer for email delivery

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -52,7 +52,7 @@ export class UsersService {
     user.passwordResetToken = hashedToken;
     user.passwordResetExpires = new Date(Date.now() + 60 * 60 * 1000);
     await this.usersRepository.save(user);
-    this.emailService.sendPasswordResetEmail(user.username, token);
+    await this.emailService.sendPasswordResetEmail(user.username, token);
   }
 
   async resetPassword(token: string, password: string): Promise<void> {
