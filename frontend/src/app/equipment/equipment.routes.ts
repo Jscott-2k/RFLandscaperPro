65faf3657af52ac3398a4d231bba4e2e64fa9e77
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-28 12:42:04 -0400

    feat: add equipment detail component and service

--- frontend/src/app/equipment/equipment.routes.ts
+++ frontend/src/app/equipment/equipment.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from '@angular/router';
 export const equipmentRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./equipment.component').then(m => m.EquipmentComponent)
-  }
+    loadComponent: () => import('./equipment-list.component').then((m) => m.EquipmentListComponent),
+  },
+  {
+    path: 'new',
+    loadComponent: () =>
+      import('./equipment-detail.component').then((m) => m.EquipmentDetailComponent),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./equipment-detail.component').then((m) => m.EquipmentDetailComponent),
+  },
 ];
