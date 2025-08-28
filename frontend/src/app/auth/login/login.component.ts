2d45651ab201a58a69457d26100e96e982082b74
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-28 18:38:56 -0400

    Route users to dashboard after auth

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -29,7 +29,7 @@ export class LoginComponent {
   submit(): void {
     if (this.form.valid) {
       this.auth.login(this.form.getRawValue()).subscribe(() => {
-        this.router.navigate(['/']);
+        this.router.navigate(['/dashboard']);
       });
     }
   }
