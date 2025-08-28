b483ad0d4338d8bec5857a39c9776726ca0e4bdc
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-28 18:50:38 -0400

    feat(customers): enable editing

--- frontend/src/app/customers/customer-detail.component.ts
+++ frontend/src/app/customers/customer-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CustomerService } from './customer.service';
 import { Customer } from './customer.model';
 import { switchMap } from 'rxjs';
@@ -8,12 +8,13 @@ import { switchMap } from 'rxjs';
 @Component({
   selector: 'app-customer-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   template: `
     <ng-container *ngIf="customer$ | async as customer">
       <h2>{{ customer.name }}</h2>
       <p>Email: {{ customer.email }}</p>
       <p *ngIf="customer.phone">Phone: {{ customer.phone }}</p>
+      <a [routerLink]="['/customers', customer.id, 'edit']">Edit</a>
     </ng-container>
   `
 })
