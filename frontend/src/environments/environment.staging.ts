ab14c438f25ff73ed61083694786b9d05b0d33d1
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-28 18:23:21 -0400

    fix: guard environment variables in browser

--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -1,4 +1,8 @@
+const apiUrl =
+  (typeof process !== 'undefined' && process.env['API_URL']) ||
+  'https://staging.rflandscaperpro.com/api';
+
 export const environment = {
   production: false,
-  apiUrl: process.env['API_URL'] || 'https://staging.rflandscaperpro.com/api'
+  apiUrl,
 };
