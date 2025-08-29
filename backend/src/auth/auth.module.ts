d58a67827837c73d1010fc0d2f115f852d09f4a8
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-29 14:42:04 -0400

    refactor auth signup owner

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -8,16 +8,14 @@ import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './jwt.strategy';
 import { RefreshToken } from './refresh-token.entity';
 import { VerificationToken } from './verification-token.entity';
-import { User } from '../users/user.entity';
 import { EmailService } from '../common/email.service';
-import { Company } from '../companies/entities/company.entity';
 import { CompanyUser } from '../companies/entities/company-user.entity';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule,
-    TypeOrmModule.forFeature([RefreshToken, VerificationToken, User, Company, CompanyUser]),
+    TypeOrmModule.forFeature([RefreshToken, VerificationToken, CompanyUser]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
