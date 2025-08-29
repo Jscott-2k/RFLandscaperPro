b92d1b8d8d95bb88630e7c017b6102dae20aedbc
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-29 16:18:57 -0400

    Specify server rendering for dynamic routes

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,30 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'customers/:id/edit',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'customers/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'equipment/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'jobs/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'contracts/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'users/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
