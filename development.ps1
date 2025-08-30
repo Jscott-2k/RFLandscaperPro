5e3630b3c8616959213c784207e996279de1d0bb
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-29 22:06:38 -0400

    feat: display service endpoints in dev scripts

--- development.ps1
+++ development.ps1
@@ -2,3 +2,15 @@
 $ErrorActionPreference = "Stop"
 
 docker compose up -d
+
+Write-Host @" 
+Services are available at the following endpoints:
+  Frontend:   http://localhost:4200
+  Backend:    http://localhost:3000
+  Swagger UI: http://localhost:3000/docs
+  Log Server: http://localhost:9880
+  Mailhog UI: http://localhost:8025 (SMTP: localhost:1025)
+  Database:   localhost:5432
+  Prometheus: http://localhost:9090
+  Grafana:    http://localhost:3001
+"@
