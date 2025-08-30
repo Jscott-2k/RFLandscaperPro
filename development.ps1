de6220fdee56f36a7782bc2386fc3edabbfb954c
Justin Scott <36645699+Jscott-2k@users.noreply.github.com>
2025-08-29 22:09:01 -0400

    fix: make dev script endpoints clickable

--- development.ps1
+++ development.ps1
@@ -3,14 +3,12 @@ $ErrorActionPreference = "Stop"
 
 docker compose up -d
 
-Write-Host @" 
-Services are available at the following endpoints:
-  Frontend:   http://localhost:4200
-  Backend:    http://localhost:3000
-  Swagger UI: http://localhost:3000/docs
-  Log Server: http://localhost:9880
-  Mailhog UI: http://localhost:8025 (SMTP: localhost:1025)
-  Database:   localhost:5432
-  Prometheus: http://localhost:9090
-  Grafana:    http://localhost:3001
-"@
+Write-Host "Services are available at the following endpoints:"
+Write-Host "Frontend: http://localhost:4200"
+Write-Host "Backend: http://localhost:3000"
+Write-Host "Swagger UI: http://localhost:3000/docs"
+Write-Host "Log Server: http://localhost:9880"
+Write-Host "Mailhog UI: http://localhost:8025 (SMTP: smtp://localhost:1025)"
+Write-Host "Database: postgres://localhost:5432"
+Write-Host "Prometheus: http://localhost:9090"
+Write-Host "Grafana: http://localhost:3001"
